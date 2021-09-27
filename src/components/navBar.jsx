afcc06b7919159c641d7a7b91d8725eeff4f1ccf
Nero28 <shackiy.s@yandex.ru>
2021-09-27 14:30:03 +0300

    removal of functions in app.jsx

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -3,21 +3,6 @@ import React from "react";
 const NavBar = ({ totalItems }) => {
   return (
     <ul className="nav">
-      {/* <li className="nav-item">
-        <a className="nav-link active" aria-current="page" href="/">
-          Active
-        </a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="/">
-          Link
-        </a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="/">
-          Link
-        </a>
-      </li> */}
       <li className="nav-item">
         <a className="nav-link position-relative" href="/">
           Корзина
